88d5377068c8851945026c6ccc86ac08998ff2cb
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-06-19 19:13:04 +1200

    Capital done

--- code/4-capital.js
+++ code/4-capital.js
@@ -4,4 +4,19 @@
 
 function capital() {
   // write your code here
+
+  alert("What's your name?")
+  let name = prompt ()
+
+alert('Quick question'+name+'What is the capital of France?')
+let capital=prompt ()
+
+  if(capital== 'Paris')
+  alert("That's right"+ name +'!')
+
+  else{(capital!= 'Paris')
+alert('Sorry'+ name +'that is not correct!')
+}
+alert('Goodbye!')
+
 }
