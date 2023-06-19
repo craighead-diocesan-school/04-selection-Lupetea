55e4c7681092684168e3a4c9aed464a0acc3625e
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-06-19 18:57:15 +1200

    conversation done

--- code/2-conversation.js
+++ code/2-conversation.js
@@ -3,20 +3,28 @@
 // ####################################
 
 function conversation() {
-  alert('Welcome to my conversation program.')
+  alert('Welcome to my school work conversation program.')
 
-  alert('Do you like cycling? Answer yes or no.')
+  alert('Do you like Mathematics? Answer yes or no.')
   let answer = prompt()
 
-  if (answer == 'yes') {
-    alert("That's good - you will get very fit.")
-  } else {
-    alert('Perhaps you like some other sport.')
+  if (answer == 'yes')
+  alert('Woahh, Awesome!')
+
+  else{ (answer == 'no')
+  alert('It can be quite tricky.')
   }
 
-  alert('Goodbye')
+alert ('Do you like English?')
+let english= prompt ()
+
+if (english== 'yes')
+alert("That's good for you!")
+
+else{
+(english == 'no')
+alert('That is understandable.')
 }
 
-function age() {
-  // write your code here
+  alert('Goodbye')
 }
