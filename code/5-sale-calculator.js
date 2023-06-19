098a2e2671de800ac8a3092e16adbffc8eda4240
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-06-19 19:31:45 +1200

    Sale calculator done!

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -4,4 +4,22 @@
 
 function saleCalculator() {
   // write your code here
+
+  alert('How much money do you have to spend?')
+  let money= prompt ()
+
+  alert('How much is the item normally?')
+  let price=prompt ()
+
+  alert('What is the discount percentage?')
+  let discount=prompt ()
+
+  let total=(price-discount)
+  if (price-discount>money)
+  alert("You can't afford to buy it as you have only"+money+'to spend, but even with'+discount+'off'+price+'only comes down to'+total+'!')
+
+  else{(price-discount<money)
+  alert('You can afford it, as the'+discount+'off the'+price+'only comes down to'+total+ 'and you have'+money+'dollars to spend.')
+}
+alert('Goodbye!')
 }
