63717569f7a4ada8bbfe90d4694ab8b41665b001
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-06-19 19:04:35 +1200

    Is it more than 2 is done

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -4,4 +4,21 @@
 
 function moreThanTwo() {
   // write your code here
-}
+
+  alert('What number do you wish to compare to 2?')
+  let number = prompt()
+
+  if (number>'2')
+  alert(number+ 'is greater than 2')
+
+if(number == '2')
+alert(number+ 'is equal to 2')
+
+  else {
+    (number < '2')
+    alert(number+'is smaller than 2')
+  }
+alert('Goodbye!')
+
+  }
+
